62977f3b258289ee46b4127930a0865afadeca8b
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-18 13:35:15 +0530

    Add navigation bar component

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Homepage = () => {
+  return <div>Currency Homepage</div>;
+};
+
+export default Homepage;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FaMicrophone, FaRegSun, FaSearch } from "react-icons/fa";
+import "./navbar.css";
+
+const Navbar = () => (
+  <div>
+    <nav className="navbar navbar-expand-lg ">
+      <a className="navbar-brand fontcolor" href="/">
+        Navbar
+      </a>
+
+      <ul className="navbar-nav mr-auto d-flex flex-column">
+        <li className="nav-item activen d-flex">
+          <a className="nav-link" href="/">
+            <FaMicrophone className="fontcolor" />
+            <FaRegSun className="ms-4 me-2 fontcolor" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <form className="form-inline ">
+      <input type="search" placeholder="Search" aria-label="Search"></input>
+      <FaSearch className="search-icon" />
+    </form>
+  </div>
+);
+
+export default Navbar;
