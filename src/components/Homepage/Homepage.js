8b005e4d256851306018a0dc89ee91768265b75e
Megha Bodke <megha.n.bodke@gmail.com>
2022-10-19 13:32:52 +0530

    Add style to home page

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
+import React from "react";
+import { FaArrowCircleRight } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import "./Homepage.css";
 const Homepage = () => {
   // allcoins comes from reducer alias in configure store.
   const { coins } = useSelector((state) => state.allcoins);
   console.log(`this store state ${coins}`);
   return (
-    <div>
+    <div className="row">
       {coins.map((coin) => (
-        <p key={Math.random()}>{coin.name}</p>
+        <>
+          <div className="col-6 d-flex flex-column">
+            <a>
+              <FaArrowCircleRight />
+            </a>
+            <img src={coin.image} alt="coin image" className="coin-image" />
+            <h2 key={Math.random()}>{coin.name}</h2>
+            <h5>{coin.symbol}</h5>
+          </div>
+        </>
       ))}
     </div>
   );
